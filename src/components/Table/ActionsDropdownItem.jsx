7b0e77f5ee623d96efeb05492acf9d373480804d
Klapzo <86894724+Klapzo@users.noreply.github.com>
2023-09-02 09:59:44 -0300

    fixed color props

--- src/components/Table/ActionsDropdownItem.jsx
+++ src/components/Table/ActionsDropdownItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ActionsDropdownItem ({ icon, text, color }) {
   return (
-      <span className={`text-sm flex flex-row items-center gap-2 text-${color}-400 cursor-pointer active:opacity-50`}>
+      <span className={`text-sm flex flex-row items-center gap-2 text-${color} cursor-pointer active:opacity-50`}>
           {icon} {text}
       </span>
   )
