a9de438b55f6e81832cb783dc757e41810840ad3
Klapzo <86894724+Klapzo@users.noreply.github.com>
2023-09-05 15:30:14 -0300

    fixed tooltip prop

--- src/components/Table/ActionsDropdownItem.jsx
+++ src/components/Table/ActionsDropdownItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function ActionsDropdownItem ({ icon, text, color, tooltip }) {
+function ActionsDropdownItem ({ icon, text, color }) {
   return (
       <div className="relative flex items-center gap-2">
           <span className={`text-sm flex flex-row items-center gap-2 text-${color} cursor-pointer active:opacity-50`}>
